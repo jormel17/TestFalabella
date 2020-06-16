1dcf3aa2b321b5d6f35945052736642a826853a6
jormel figueroa <jormel17@gmail.com>
2020-06-16 19:50:13 -0400

    Test falabella

--- src/test/java/Test/AddProductCarTest.java
+++ src/test/java/Test/AddProductCarTest.java
@@ -15,9 +15,10 @@ import java.io.IOException;
 
 public class AddProductCarTest extends Base {
 
+
     HomePage pageHome;
     AddProductCarPage pageAddProductCar;
-    String brand = "SONY";
+    String brand = "sony";
     String expectResultConsola = "Consolas";
     String expectResultBrand = "SONY";
     String expectBuyResume = "Resumen de tu orden";
@@ -29,7 +30,7 @@ public class AddProductCarTest extends Base {
     }
 
     @Test
-    public void Navigation() {
+    public void Navigation() throws InterruptedException {
 
         pageHome = new HomePage(driver);
         pageHome.clickCategory();
@@ -39,8 +40,8 @@ public class AddProductCarTest extends Base {
 
         pageAddProductCar = new AddProductCarPage(driver);
         pageAddProductCar.clickBrand();
-
         pageAddProductCar.selectChecboxBrand(brand);
+        Thread.sleep(5000);
         Assert.assertEquals(pageAddProductCar.getBrandFilter(), expectResultBrand);
         pageAddProductCar.clickBtnSeePorduct();
         Assert.assertTrue(pageAddProductCar.getBtnAddBag().isDisplayed());
